c5110b87929167940d55d9ae7bd8c764e911691c
Ankit <ankit.bansal_cs17@gla.ac.in>
2019-03-11 15:31:56 +0530

    display,insertfirst,insertlast maethod made

--- src/two.java
+++ src/two.java
@@ -23,6 +23,11 @@ class Nodee{
     private Student data;
     private Nodee next;
 
+    public Nodee() {
+        data.setRollno(0);
+        data.setName(null);
+        next=null;
+    }
     public Nodee(int Rollno,String Name) {
         data.setRollno(Rollno);
         data.setName(Name);
@@ -36,7 +41,82 @@ class Nodee{
     public void setNext(Nodee next) {
         this.next = next;
     }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
 }//end of nodee
+
+class MyCircularListNonPrimitive{
+    Nodee Start;
+
+    public MyCircularListNonPrimitive() {
+        Start = null;
+    }
+
+    public boolean isempty(){
+        if(Start==null)
+            return true;
+        else
+            return false;
+    }//end of method isempty
+
+    public void display(){
+        if(isempty()) {
+            System.out.println("list empty");
+            return;
+        }//end of if
+        else{
+            Nodee temp=Start;
+            while(temp.getNext()!=Start){
+                System.out.println(temp.getData());
+                temp=temp.getNext();
+            }//end of while
+            System.out.println(temp.getData());
+        }//end of else
+    }//end of method display
+
+    public void insertend(Student val){
+        Nodee n=new Nodee();
+        n.setData(val);
+        if(isempty()){
+            Start=n;
+            n.setNext(n);  //n.setNext(Start);
+        }//end of if
+        else{
+            Nodee temp=Start;
+            while(temp.getNext()!=Start){
+                temp=temp.getNext();
+            }
+            temp.setNext(n);
+            n.setNext(Start);
+        }//end of else
+    }//end of method insertend
+
+    public void insertfirst(Student val){
+        Nodee n=new Nodee();
+        n.setData(val);
+        if(isempty()){
+            insertend(val);
+        }//end of if
+        else{
+            Nodee temp=Start;
+            n.setNext(Start);
+            while(temp.getNext()!=Start){
+                temp=temp.getNext();
+            }//end of while
+            temp.setNext(n);
+            Start=n;
+        }//end of else
+    }//end of method insertfirst
+
+
+
+}//end of class MyCircularListNonPrimitive
 
 
 public class two {
