db65fe299a8bef947c2a06c232b669d74a790048
Ankit <ankit.bansal_cs17@gla.ac.in>
2019-03-11 15:21:47 +0530

    add nonprimitive method student

--- src/two.java
+++ src/two.java
@@ -1,3 +1,25 @@
+class Student{
+    int Rollno;
+    String Name;
+    //getter & Setter Methods
+    public int getRollno() {
+        return Rollno;
+    }
+
+    public void setRollno(int rollno) {
+        Rollno = rollno;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}//end of non-primitive datatype Student
+
+
 
 
 public class two {
